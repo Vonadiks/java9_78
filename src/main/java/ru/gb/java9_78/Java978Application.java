e58fee793acedcc39bdd26ab9342ddb4365bedbc
Sergey Skidanov <vonadiks@mail.ru>
2021-12-12 00:43:08 +0400

    initial commit

---
+++ src/main/java/ru/gb/java9_78/Java978Application.java
@@ -0,0 +1,13 @@
+package ru.gb.java9_78;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java978Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java978Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/gb/java9_78/Java978ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.java9_78;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java978ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
